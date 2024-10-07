ccc3726c3102c0bcf918f2bd546b3c848acea6f1
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-07 07:23:45 +0100

    startup task

---
+++ StartUpTasks.js
@@ -0,0 +1,38 @@
+let x = 2;
+let y = 3;
+
+let doubleSum = 2 * (x + y);
+console.log("Double sum = " + doubleSum);
+
+let number = 145;
+let remainder = number % 10;
+let sum = remainder + number;
+console.log("sum = " + sum);
+
+let a = 3;
+let n = 4;
+let value = 0;
+
+for(let i = 0; i < n; i++){
+if(n % i == 0) value++;
+}
+
+console.log("value = " + value *3 );
+
+const b = 2;
+
+let v = b + 10;
+
+let w = b - 10;
+
+let z = b % 10;
+
+let product = v * w * z;
+
+let division = b/product;
+
+console.log("division = " + division);
+console.log("product = " + product);
+
+
+
