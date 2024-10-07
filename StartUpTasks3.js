1dc3c1de2e443ddb4b2eb7c6452ccb5847f1f455
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-07 07:24:59 +0100

    start up 3

---
+++ StartUpTasks3.js
@@ -0,0 +1,45 @@
+let number = 10
+let count = 0
+
+while (count < number){ count++;
+console.log(count);
+}
+
+/*do{
+	console.log(count);
+	count++;
+	}
+while(count < number)
+*/
+
+let arr = [1,2,3, "boy"];
+
+let user = {
+	age : 12,
+	sex: "male",
+
+};
+
+//user is an object
+
+for(let key in user){
+	console.log(user[key]);
+
+};
+
+for(let index in arr){
+	//console.log(index);
+	console.log(arr[index]);
+}
+
+let word = "chinedu";
+
+for (let char of word){
+	console.log(char);
+
+	  }
+
+let array = [1,2,"dog","hamster"];
+for(let element of array){
+	console.log([element]);
+}
